5dd44d732ec90ce4b8dc0aa206a4da6fc32ea1ce
ezbiamb <ambrozinski@gmail.com>
2016-06-12 14:20:39 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 8 cze 2016
+ *      Author: zybdzghfryd
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello!" << endl;
+    return 0;
+}
