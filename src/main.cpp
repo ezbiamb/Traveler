a3cf3f0f4508669bfa2b8baa4c867f379f98f6e0
ezbiamb <ambrozinski@gmail.com>
2016-07-27 22:06:34 +0200

    Fixed semicolon after cout

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 int main() {
     cout << "Hello world and other worlds!" << endl;
     cout << "Kocham mojom Merdziem!!!!!!!" << endl;
-    cout << "If I get it, I'm on auto testing!"
+    cout << "If I get it, I'm on auto testing!" << endl;
     Echo A(1);
     cout << A.echo() << endl;
     return 0;
